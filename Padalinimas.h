565d1da3574147f8cfffa0529fde885ac1a39590
Guoda25 <112932535+Guoda25@users.noreply.github.com>
2022-12-04 20:20:45 +0200

    Update Padalinimas.h

--- Padalinimas.h
+++ Padalinimas.h
@@ -1,5 +1,3 @@
 #include "Lib.h"
 
 void studentu_isskaidymas(vector<Duomenys>& studentas, vector<Duomenys>& vargsiukai, vector<Duomenys>& kietiakai);
-
-void studentu_isskaidymas2(list<Duomenys>& studentas, list<Duomenys>& vargsiukai);
