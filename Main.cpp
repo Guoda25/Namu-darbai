f89a589d69ecfa0b796d51c4ed6ae07138defbab
Guoda25 <112932535+Guoda25@users.noreply.github.com>
2022-12-04 18:28:52 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #include "FailoNuskaitymas.h"
 #include "tryCatch.h"
-void vidurkis(Duomenys studentas[], int i);
 
 
 int main()
@@ -8,63 +7,55 @@ int main()
     int paz_sk;
     int studentu_sk;
     char temp;
-    Duomenys studentas[1];
     std::vector<Duomenys> studentai;
-   
- 
-
 
     do
     {
-        std::cout << "Jeigu norite, kad studentu pazymiai butu suvesti automatiskai, iveskite \"A\"\nJeigu studentu pazymius norite suvesti ranka, iveskite \"R\"\nJeigu norite, kad studentu pazymiai butu nuskaityti is failo, iveskite \"F\"\n";
+        std::cout << "Jeigu norite, kad studentu pazymiai butu suvesti automatiskai, iveskite \"A\"\n"
+            << "Jeigu studentu pazymius norite suvesti ranka, iveskite \"R\"\n"
+            << "Jeigu norite, kad studentu pazymiai butu nuskaityti is failo, iveskite \"F\"\n";
         std::cin >> temp;
-        if (temp != 'a' && temp != 'A' && temp != 'r' && temp != 'R' && temp != 'f' && temp != 'F') { std::cout << "pakartokite, netinkamas simbolis\n"; }
+        if (temp != 'a' && temp != 'A' && temp != 'r' && temp != 'R' && temp != 'f' && temp != 'F') { 
+            std::cout << "pakartokite, netinkamas simbolis\n"; }
+
     } while (temp != 'a' && temp != 'A' && temp != 'r' && temp != 'R' && temp != 'f' && temp != 'F');
 
     if (temp == 'a' || temp == 'A' || temp == 'r' || temp == 'R') {
         do
         {
-            std::cout << "Iveskite studentu kieki:\n";
+            std::cin.clear();
+            std::cin.ignore();
+            std::cout << "Iveskite studentu skaiciu:\n";
             std::cin >> studentu_sk;
-        } while (int(studentu_sk) < 0 || int(studentu_sk) > 256);
+        } while (studentu_sk < 0 || studentu_sk > 256 || std::cin.fail());
     }
 
     switch (temp) {
     case 'a':
     case 'A':
        
+        studentai.resize(studentai.size() + studentu_sk);
 
         for (int i = 0; i < studentu_sk; i++)
-        {   
-            
-            vardas(studentas, i);
-            std::cout << "Iveskite " << i + 1 << "-ojo studento pazymiu skaiciu: ";
-            std::cin >> paz_sk;
-            ivedimas_automatiskai(studentas, tryCatch(paz_sk));
-            vidurkis(studentas, 0);
-            studentai.push_back(studentas[0]);
-
+        {
+            ivedimas_automatiskai(studentai, i);
         }
+
         //rezultatai(studentai);
         rezultatau_isvedimas_iFaila(studentai);
-        std::cout << std::endl <<"***STUDENTU REZULTATUS RASITE FAILE 'kursiokai.txt'***" << std::endl;
         break;
 
     case 'r':
     case 'R':
         
+        studentai.resize(studentai.size() + studentu_sk);
+
         for (int i = 0; i < studentu_sk; i++)
         {
-            vardas(studentas, i);
-            std::cout << "Iveskite " << i + 1 << "-ojo studento pazymiu skaiciu: ";
-            std::cin >> paz_sk;
-            ivedimas_ranka(studentas, tryCatch(paz_sk));
-            vidurkis(studentas, 0);
-            studentai.push_back(studentas[0]);
+            ivedimas_ranka(studentai, i);
         }
         //rezultatai(studentai);
         rezultatau_isvedimas_iFaila(studentai);
-        std::cout << std::endl << "***STUDENTU REZULTATUS RASITE FAILE 'kursiokai.txt'***" << std::endl;
         break;
 
     case 'f':
@@ -72,7 +63,6 @@ int main()
         failo_nuskaitymas(studentai, &paz_sk);
         //rezultatai(studentai);
         rezultatau_isvedimas_iFaila(studentai);
-        std::cout << std::endl << "***STUDENTU REZULTATUS RASITE FAILE 'kursiokai.txt'***" << std::endl;
         break;
 
     }
